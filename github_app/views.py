288b186c8a60541ad468cace8795e0d9ecb4c6a6
TimVanHal <tim.vanhal@student.ap.be>
2016-06-11 18:09:45 +0200

    Added top 10 functionality
    Temporarily hard-coded for a specific repository

--- github_app/views.py
+++ github_app/views.py
@@ -17,8 +17,52 @@ def get_commits(repo):
 		com['name'] = committer['name']
 		com['email'] = committer['email']
 		com['message'] = commit['commit']['message']
+		com['sha'] = commit['sha']
 		commit_list.append(com)
 	return commit_list
+
+def get_files(repo, sha):
+	data = {}
+	file_list = []
+	get_url = base_url + repo + '/commits/' + sha
+	response = urllib2.urlopen(get_url)
+	data = json.load(response)
+	files = data['files']
+	for file in files:
+		filedata = {}
+		filedata['name'] = file['filename']
+		filedata['status'] = file['status']
+		file_list.append(filedata)
+	return file_list
+
+def get_top_ten(repo):
+	commits = get_commits(repo)
+	files = []
+	for commit in commits:
+		sha = commit['sha']
+		temp_files = get_files(repo, sha)
+		for file in temp_files:
+			files.append(file)
+	changelist = count_commits(files)
+	sorted_list = sorted(changelist, key=lambda k : k['count'])[::-1]
+	sorted_list = sorted_list[:10]
+	return sorted_list
+
+
+def count_commits(files):
+	uniquefiles = []
+	changelist = []
+	for file in files:
+		if file['name'] not in uniquefiles:
+			uniquefiles.append(file['name'])
+			temp = {}
+			temp['name'] = file['name']
+			temp['count'] = 1
+			changelist.append(temp)
+		else: # File is modified.
+			listed_file = filter(lambda lfile: lfile['name'] == file['name'], changelist)[0]
+			listed_file['count'] += 1
+	return changelist
 
 # Create your views here.
 def index(request):
@@ -37,4 +81,6 @@ def list(request):
 	return render(request, 'github_app/list.html')
 
 def detail(request):
-	return HttpResponse("details")
+	repo = 'TimVanHal/webtech3_taak'
+	top = get_top_ten(repo)
+	return render(request, 'github_app/detail.html', {'files' : top})
